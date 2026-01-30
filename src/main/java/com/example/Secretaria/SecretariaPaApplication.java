6c7f1fb22909d9d42eac565ca8eb2ac68351df7d
Alisson1471 <alissonalvesdossantosoliveira@gmail.com>
2026-01-30 10:33:50 -0300

    initial commit

---
+++ src/main/java/com/example/Secretaria/SecretariaPaApplication.java
@@ -0,0 +1,13 @@
+package com.example.Secretaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecretariaPaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecretariaPaApplication.class, args);
+	}
+
+}
