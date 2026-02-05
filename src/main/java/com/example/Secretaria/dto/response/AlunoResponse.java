ee2eb1623ebc08d27b5f2bcb0a4163f8404234be
CaduPerrudGerminare <carlos.sousa@germinare.org.br>
2026-02-05 09:22:28 -0300

    feat: implement full Aluno CRUD

--- src/main/java/com/example/Secretaria/dto/response/AlunoResponse.java
+++ src/main/java/com/example/Secretaria/dto/response/AlunoResponse.java
@@ -1,6 +1,6 @@
 package com.example.Secretaria.dto.response;
 
-public class AlunoResponse {
+public class AlunoResponse extends UserResponse {
     private int id;
 
     private String nome;
@@ -10,4 +10,50 @@ public class AlunoResponse {
     private String cpf;
 
     private String matricula;
+
+    private String senha;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
 }
--- src/main/java/com/example/Secretaria/strategy/AlunoLoginStrategy.java
+++ src/main/java/com/example/Secretaria/strategy/AlunoLoginStrategy.java
@@ -1,15 +1,28 @@
 package com.example.Secretaria.strategy;
 
+import com.example.Secretaria.adapter.AlunoAdapter;
 import com.example.Secretaria.dto.request.LoginRequest;
 import com.example.Secretaria.dto.response.UserResponse;
+import com.example.Secretaria.mapper.AlunoMapper;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AlunoLoginStrategy implements LoginStrategy {
+    private final AlunoAdapter alunoAdapter;
+
+    private final AlunoMapper alunoMapper;
+
+    public AlunoLoginStrategy(AlunoAdapter alunoAdapter, AlunoMapper alunoMapper) {
+        this.alunoAdapter = alunoAdapter;
+        this.alunoMapper = alunoMapper;
+    }
+
     @Override
     public UserResponse login(LoginRequest loginRequest) {
-        return null;
+        var aluno = alunoAdapter.login(loginRequest.getCpf(), loginRequest.getPassword());
+        return alunoMapper.convertToResponse(aluno);
     }
+
 
     @Override
     public boolean supports(String userType) {
