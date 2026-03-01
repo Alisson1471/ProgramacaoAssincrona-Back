01814c0fffedf6a796c710f9f0e959c139c5d8fe
Alisson1471 <alissonalvesdossantosoliveira@gmail.com>
2026-03-01 20:13:18 -0300

    feat: add field "disciplina" in NotaResponseDTO

--- src/main/java/com/example/Secretaria/repository/NotaRepository.java
+++ src/main/java/com/example/Secretaria/repository/NotaRepository.java
@@ -6,6 +6,7 @@ import com.example.Secretaria.dto.response.StatusDisciplineResponse;
 import com.example.Secretaria.model.Nota;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -18,8 +19,16 @@ public interface NotaRepository extends JpaRepository<Nota, Integer> {
     @Query(value = "SELECT * FROM fn_status_disciplinas()", nativeQuery = true)
     List<StatusDisciplineResponse> findStatusDiscipline();
 
-    @Query(value = "SELECT titulo, valor FROM nota WHERE aluno_id = :aluno_id", nativeQuery = true)
-    List<NotaResponseDTO> findByAluno_Id(int aluno_id);
+    @Query(value = """
+        SELECT n.titulo,
+               d.nome AS disciplina,
+               n.valor
+        FROM nota n
+        JOIN disciplina d ON d.id = n.disciplina_id
+        WHERE n.aluno_id = :aluno_id
+        """,
+            nativeQuery = true)
+    List<NotaResponseDTO> findByAluno_Id(@Param("aluno_id") int aluno_id);
 
     @Query(value = "SELECT * FROM fn_status_disciplina_by_aluno(:aluno_id)", nativeQuery = true)
     List<NotaDisciplinaAlunoResponse> findStatusDisciplinaByAlunoId(int aluno_id);
