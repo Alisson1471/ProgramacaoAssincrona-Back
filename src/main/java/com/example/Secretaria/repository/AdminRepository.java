d7b4ed4c16d4d4fee04470cfe0ea70eaa0a4bd3c
Alisson1471 <alissonalvesdossantosoliveira@gmail.com>
2026-03-01 22:17:13 -0300

    feat: adjust some problems of api

--- src/main/java/com/example/Secretaria/repository/AdminRepository.java
+++ src/main/java/com/example/Secretaria/repository/AdminRepository.java
@@ -7,8 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface AdminRepository extends JpaRepository<Admin, Integer> {
-    Optional<Professor> criarProfessor(Professor professor);
-    Void aprovarAluno(Professor professor);
     Optional<Admin> findByCpfAndSenha(String cpf, String senha);
     Optional<Admin> findById(Integer id);
 }
